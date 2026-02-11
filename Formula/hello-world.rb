8188b04f0250cdc7864c64a24ef87476fc831b71
Brendan Fattig <brendan.fattig@nasa.gov>
2026-02-11 14:24:06 -0600

    hello-world bump revision

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -3,7 +3,8 @@ class HelloWorld < Formula
   homepage "https://github.com/brendan-nasa/hello-world"
   url "https://github.com/brendan-nasa/hello-world/archive/refs/tags/v0.1.tar.gz"
   sha256 "9350d8d8bba408dbd3f4b80160daf116b0235f4627d32845bc14dd519b5356ff"
-  license ""
+  license "MIT"
+  revision 1
 
   depends_on "cmake" => :build
 
