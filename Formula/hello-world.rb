5030788e2f59783936ef7a89f8f0ea4aed1e238b
brendan <227456334+brendan-nasa@users.noreply.github.com>
2026-02-11 20:47:33 +0000

    hello-world: add 0.1_1 bottle.

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -6,6 +6,13 @@ class HelloWorld < Formula
   license "MIT"
   revision 1
 
+  bottle do
+    root_url "https://github.com/brendan-nasa/homebrew-brew/releases/download/hello-world-0.1_1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "08097f5b41f189ef396691040a3eca8dd9b02ebb8ee1ec405b91f9ca965c8bdc"
+    sha256 cellar: :any_skip_relocation, sequoia:      "390660da984279951431ae7f4d2a47d66d678f2443d4f044bd3f8f79f10fca8b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "476a7d274806fd563ed725393794b0a24dc79eb21761a3e0ee41bde70d9a6bb3"
+  end
+
   depends_on "cmake" => :build
 
   def install
