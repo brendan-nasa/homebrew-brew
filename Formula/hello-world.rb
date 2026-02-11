7828acaff31f38cbb2394d63ce02533a52a6413a
Brendan Fattig <brendan.fattig@nasa.gov>
2026-02-11 14:40:14 -0600

    bump whoops
    Closes #1.
    
    Signed-off-by: brendan <227456334+brendan-nasa@users.noreply.github.com>

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -4,7 +4,7 @@ class HelloWorld < Formula
   url "https://github.com/brendan-nasa/hello-world/archive/refs/tags/v0.1.tar.gz"
   sha256 "9350d8d8bba408dbd3f4b80160daf116b0235f4627d32845bc14dd519b5356ff"
   license "MIT"
-  revision 2
+  revision 1
 
   depends_on "cmake" => :build
 
